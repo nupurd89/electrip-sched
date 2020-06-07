71e82d91396aa3ecc9cc8454a1c8a56437fdfd74
Nupur Dave <nupurdave89@gmail.com>
2020-06-07 15:23:22 -0500

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-Ride scheduling
-
-Go to http://nupurd89.github.io/electrip-sched/
-=======
-booking software for drivers
-
-visit: http://nupurd89.github.io/electrip-sched/
->>>>>>> fe845ec7fe66300d9e21328f9d78b29691781d8a
---
+++ precache-manifest.41faa51ecc4a2ab6c947e89db044656a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e3e9e87c481ece802949021adf40d5",
+    "url": "/electrip-sched/index.html"
+  },
+  {
+    "revision": "81a879bf61693a92dc6f",
+    "url": "/electrip-sched/static/css/2.6db02f45.chunk.css"
+  },
+  {
+    "revision": "4417835c109638128549",
+    "url": "/electrip-sched/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "81a879bf61693a92dc6f",
+    "url": "/electrip-sched/static/js/2.83e12ea4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/electrip-sched/static/js/2.83e12ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4417835c109638128549",
+    "url": "/electrip-sched/static/js/main.6588710e.chunk.js"
+  },
+  {
+    "revision": "b96b7eb19746ff66a3fb",
+    "url": "/electrip-sched/static/js/runtime-main.9fd98fc8.js"
+  }
+]);
